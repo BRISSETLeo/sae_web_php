00ba53ef7bf556f01a287307958bb775e159275e
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-02-01 11:32:33 +0100

    pr

--- config.php
+++ config.php
@@ -5,4 +5,13 @@ Autoloader::register();
 
 session_start();
 
+require '_inc/autoloader.php'; 
+Autoloader::register(); 
+
+use classes\templates;
+use classes\dataloadersqlite;
+
+$template = new Templates('./_inc/templates/template.php');
+$pdo = new DataLoaderSqlite();
+
 ?>
