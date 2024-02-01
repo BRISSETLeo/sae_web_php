c936ce64b46fa1de55f0509c549fbec50a035f8e
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-02-01 09:12:11 +0100

    modification de l'arborescence

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-require './config.php';
-=======
-session_start();
-
 require_once('./config.php');
->>>>>>> a947e4c (re structuration de l'arborescence du code)
 
 use classes\Template;
 $template = new Template('./_inc/templates/template.php');
