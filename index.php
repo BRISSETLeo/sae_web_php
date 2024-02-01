d93cf1b7783a46983d6f85664ddfe4d7cea5faa2
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-02-01 12:01:22 +0100

    pr

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])){
 
     if($action === 'connexion'){
         $template->render("./vues/connexion.php");
-        return;
+        exit;
     }else if($action === 'deconnexion'){
         session_destroy();
     }
