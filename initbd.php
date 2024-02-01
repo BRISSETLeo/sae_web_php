3e607cffd9e8035455d39dbcc9e07dd02eed1928
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-02-01 12:21:31 +0100

    methode pour initialiser la bd

---
+++ initbd.php
@@ -0,0 +1,44 @@
+<?php 
+
+require './config.php'; 
+
+use classes\dataloadersqlite;
+
+$pdo = new DataLoaderSqlite();
+
+$yamlContent = file_get_contents('./data/fixtures/extrait.yml');
+$entries = explode('- by:', $yamlContent);
+$results = [];
+
+array_shift($entries);
+
+foreach ($entries as $entry) {
+    $lines = explode(PHP_EOL, trim($entry));
+
+    $data = [];
+
+    foreach ($lines as $line) {
+        if(strpos($line, ':') === false){
+            $data['by'] = $line;
+        }
+        if (!empty($line) && strpos($line, ':') !== false) {
+            list($key, $value) = array_map('trim', explode(':', $line, 2));
+            $data[$key] = trim($value);
+        }
+    }
+
+    $results[] = $data;
+}
+
+foreach ($results as $result) {
+    $by = $result['by'];
+    $entryId = $result['entryId'];
+    $genre = $result['genre'];
+    $img = $result['img'];
+    $parent = $result['parent'];
+    $releaseYear = $result['releaseYear'];
+    $title = $result['title'];
+}
+
+header("location: ./");
+exit;
