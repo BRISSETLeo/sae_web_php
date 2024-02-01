6204b110f67d667846fa32a47b59032a726fce41
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-02-01 16:04:19 +0100

    LOGO + CSS de la page de connexion

--- vues/connexion.php
+++ vues/connexion.php
@@ -1,41 +1,32 @@
-<link rel="stylesheet" href="./static/css/identification.css" />
-<div id="identification">
-    <form id="inscription" class="formulaire-identification" action="./identification.php" method="POST">
-        <h2>Inscription</h2>
-        <input type="text" id="inscription-input" name="inscription-input" value="inscription" hidden>
-        <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" required>
-        <br>
-        <input type="password" id="password" name="password" placeholder="Mot de passe" required>
-        <br>
-        <button class="slide-line envoyer-formulaire" type="submit">S'inscrire</button>
-        <p>Vous avez déjà un compte ? <a href="#" onclick="afficherConnexion()">Connectez-vous</a></p>
-    </form>
-    <form id="connexion" class="formulaire-identification" action="./identification.php" method="POST">
-        <h2>Connexion</h2>
-        <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" required>
-        <br>
-        <input type="password" id="password" name="password" placeholder="Mot de passe" required>
-        <br>
-        <button class="slide-line envoyer-formulaire" type="submit">Se connecter</button>
-        <p>Vous n'avez pas de compte ? <a href="#" onclick="afficherInscription()">Inscrivez-vous</a></p>
-    </form>
+<link rel="stylesheet" href="./static/css/connexion.css" />
+
+<img src="./static/images/logo.png" alt="logo" id="logo" />
+
+<div id="connexion-container">
+
+    <h1>J'ai un compte iuT'Unes</h1>
+
+    <div id="connexion">
+
+        <div id="email">
+            <label for="e-mail">Adresse e-mail ou nom d'utilisateur</label>
+            <input type="text" id="e-mail" name="e-mail" placeholder="Adresse e-mail ou nom d'utilisateur" />
+        </div>
+
+        <div id="mdp">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" placeholder="Mot de passe" />
+        </div>
+        
+        <a href="#" id="se-connecter">Se connecter</a>
+
+        <a href="#" id="mot-de-passe-oublie">Mot de passe oublié ?</a>
+
+        <p>
+            Vous n'avez pas de compte ? 
+            <a href="#" id="creer-un-compte">Créer un compte</a>
+        </p>
+
+    </div>
+
 </div>
-
-<script>
-
-    var inscription = document.getElementById("inscription");
-    var connexion = document.getElementById("connexion");
-
-    function afficherInscription() {
-        inscription.style.display = "block";
-        connexion.style.display = "none";
-    }
-
-    function afficherConnexion() {
-        inscription.style.display = "none";
-        connexion.style.display = "block";
-    }
-    
-    afficherConnexion();
-
-</script>
