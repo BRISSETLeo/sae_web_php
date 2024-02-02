645f7288a9ce08228df1191fae60b4f11b218541
ibrahim ozocak <i.ozocak45@gmail.com>
2024-02-02 23:35:46 +0100

    mise a jour template avec css et js

--- _inc/templates/accueil.php
+++ _inc/templates/accueil.php
@@ -1,47 +1,53 @@
 <?php
 
+use classes\dataloadersqlite;
+$sqlite = new dataloadersqlite();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>iuT'Unes</title>
-        <link rel="stylesheet" href="./static/css/template.css">
-    </head>
-    <body>
-        <div id="menu-gauche">
-            <div id="menu-haut">
-                <div class="logo">
-                    <a href="#">iuT'Unes</a>
-                </div>
-                <div class="acceuil">
-                    <img src="./static/images/accueil.png" alt="Maison">
-                    <a class="couleur" href="#">Accueil</a>
-                </div>
-                <div class="recherche">
-                    <img src="./static/images/rechercher.png" alt="loupe">
-                    <a class="couleur" href="#">Recherche</a>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <title>Yfitops</title>
+    <link rel="stylesheet" href="static/css/template.css">
+    <link rel="stylesheet" href="static/css/accueil.css">
+</head>
+<body>
+    <div id="menu-gauche">
+        <div id="menu-haut">
+            <div class="logo">
+                <img src="static/images/diamond.png" alt="Logo">
+                <a href="#">iuT'Unes</a>
             </div>
-            <div id="menu-corps">
-                <div class="playlist">
-                    <img src="./static/images/playlist.png" alt="playlist">
-                    <a class="couleur" href="#">Playlist</a>
-                    <img class="plus" src="./static/images/plus.png" alt="+">
-                    <img class="fleche-droite" src="./static/images/fleche-droite.png" alt="fleche vers la droite">
-                </div>
+            <div class="accueil">
+                <img src="static/images/accueil.png" alt="Maison">
+                <a href="#">Accueil</a>
+            </div>
+            <div class="recherche">
+                <img src="static/images/rechercher.png" alt="loupe">
+                <a href="#">Recherche</a>
             </div>
         </div>
-        <main id="main">
-            <header>
-                <div class="header-buttons">
-                    <a href="#" class="header-button">Inscription</a>
-                    <a href="#" class="header-button">Connexion</a>
-                </div>
-            </header>
-            <div> <?php echo $content; ?></div>
-        </main>
-    </body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <div id="menu-corps">
+            <div class="playlist">
+                <img src="static/images/playlist.png" alt="playlist">
+                <a href="#">Playlist</a>
+                <img class="plus" src="static/images/plus.png" alt="+">
+                <img class="fleche-droite" src="static/images/fleche-droite.png" alt="fleche vers la droite">
+            </div>
+        </div>
+    </div>
+    <main id="main">
+        <header>
+            <div class="header-buttons">
+                <a href="#" class="inscription">S'inscrire</a>
+                <a href="#" class="connexion">Se connecter</a>
+            </div>
+        </header>
+        <div> <?php echo $content; ?></div>
+    </main>
+</body>
+<script src="static/js/accueil.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </html>
---
+++ static/js/accueil.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var alogo = document.querySelector('.logo a');
+    var imglogo = document.querySelector('.logo img[alt="Logo"]');
+    var aaccueil = document.querySelector('.accueil a');
+    var imgaccueil = document.querySelector('.accueil img[alt="Maison"]');
+    var arecherche = document.querySelector('.recherche a');
+    var imgrecherche = document.querySelector('.recherche img[alt="loupe"]');
+    var aplaylist = document.querySelector('.playlist a');
+    var imgplalist = document.querySelector('.playlist img[alt="playlist"]');
+
+    // Fonctions de gestion des événements
+    function handleHover(a, img) {
+        img.style.opacity = a.style.opacity = 1;
+    }
+    
+    function handleMouseOut(a, img) {
+        img.style.opacity = a.style.opacity = 0.7;
+    }
+    
+    // Vérifiez si ça marche
+    function handleClick(a) {
+        // Redirigez vers l'URL du lien a
+        window.location.href = a.href;
+    }
+
+    // Event listeners sur la div logo
+    imglogo.addEventListener("click",function(){handleClick(alogo)});
+
+    // Event listeners sur la div accueil
+    aaccueil.addEventListener('mouseover', function() { handleHover(aaccueil, imgaccueil); });
+    aaccueil.addEventListener('mouseout', function() { handleMouseOut(aaccueil, imgaccueil); });
+    imgaccueil.addEventListener('mouseover', function() { handleHover(aaccueil, imgaccueil); });
+    imgaccueil.addEventListener('mouseout',function() {handleMouseOut(aaccueil, imgaccueil)});
+    imgaccueil.addEventListener('click', function() {handleClick(aaccueil)});
+    
+    // Event listeners sur la div recherche
+    arecherche.addEventListener('mouseover', function() { handleHover(arecherche, imgrecherche); });
+    arecherche.addEventListener('mouseout', function() { handleMouseOut(arecherche, imgrecherche); });
+    imgrecherche.addEventListener('mouseover', function() { handleHover(arecherche, imgrecherche); });
+    imgrecherche.addEventListener('mouseout',function() {handleMouseOut(arecherche, imgrecherche)});
+    imgrecherche.addEventListener('click', function() {handleClick(arecherche)});
+    
+    // Event listeners sur la div playlist
+    aplaylist.addEventListener('mouseover', function() { handleHover(aplaylist, imgplalist); });
+    aplaylist.addEventListener('mouseout', function() { handleMouseOut(aplaylist, imgplalist); });
+    imgplalist.addEventListener('mouseover', function() { handleHover(aplaylist, imgplalist); });
+    imgplalist.addEventListener('mouseout',function() {handleMouseOut(aplaylist, imgplalist)});
+    imgplalist.addEventListener('click', function() {handleClick(aplaylist)});
+});
