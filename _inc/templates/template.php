f656b70e0a949b26dde1fd8c9545bd73e1ade773
ibrahim ozocak <i.ozocak45@gmail.com>
2024-02-02 22:56:15 +0100

    ajout du CSS du template V1

--- _inc/templates/template.php
+++ _inc/templates/template.php
@@ -10,40 +10,42 @@ $sqlite = new dataloadersqlite();
 <head>
     <meta charset="UTF-8">
     <title>Yfitops</title>
-    <link rel="stylesheet" href="./static/css/template.css">
+    <link rel="stylesheet" href="static/css/template.css">
 </head>
 <body>
     <div id="menu-gauche">
         <div id="menu-haut">
             <div class="logo">
-                <a href="#"><img src="./static/images/logo.png" alt="logo"></a>
+                <img src="static/images/diamond.png" alt="Logo">
+                <a href="#">iuT'Unes</a>
             </div>
-            <div class="acceuil">
-                <img src="./static/images/accueil.png" alt="Maison">
+            <div class="accueil">
+                <img src="static/images/accueil.png" alt="Maison">
                 <a href="#">Accueil</a>
             </div>
             <div class="recherche">
-                <img src="./static/images/rechercher.png" alt="loupe">
+                <img src="static/images/rechercher.png" alt="loupe">
                 <a href="#">Recherche</a>
             </div>
         </div>
         <div id="menu-corps">
             <div class="playlist">
-                <img src="./static/images/playlist.png" alt="playlist">
+                <img src="static/images/playlist.png" alt="playlist">
                 <a href="#">Playlist</a>
-                <img class="plus" src="./static/images/plus.png" alt="+">
-                <img class="fleche-droite" src="./static/images/fleche-droite.png" alt="fleche vers la droite">
+                <img class="plus" src="static/images/plus.png" alt="+">
+                <img class="fleche-droite" src="static/images/fleche-droite.png" alt="fleche vers la droite">
             </div>
         </div>
     </div>
     <main id="main">
         <header>
             <div class="header-buttons">
-                <a href="#" class="header-button">Inscription</a>
-                <a href="#" class="header-button">Connexion</a>
+                <a href="#" class="inscription">S'inscrire</a>
+                <a href="#" class="connexion">Se connecter</a>
             </div>
         </header>
         <div> <?php echo $content; ?></div>
     </main>
+    <script src="static/js/template.js"></script>
 </body>
 </html>
