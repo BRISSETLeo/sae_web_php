ad86289433e422647fb376b1406d1fd35363efe8
MaximeSevellec <maxime.sevellec@gmail.com>
2024-02-09 13:11:58 +0100

    possibilite de rechercher des albums

--- static/js/accueil.js
+++ static/js/accueil.js
@@ -107,6 +107,35 @@ $.ajax({
     }
 });
 
+const monInput = document.getElementById('search-query');
+monInput.addEventListener('input', function () {
+    console.log(monInput.value);
+    $.ajax({
+        url: './getRecherche.php',
+        type: 'POST',
+        data: {
+            recherche: monInput.value
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.log("test", data);
+            document.getElementById('container-recherche').innerHTML = "";
+            for (var key in data) {
+                afficherRecherche(data[key]);
+            }
+        }
+        
+    });
+});
+
+function afficherRecherche(recherche) {
+    var div = document.createElement('div');
+    div.innerHTML = `
+    <h6>${recherche["name"]}</h6>
+    `;
+    document.getElementById('container-recherche').appendChild(div);
+}
+
 function afficherAlbum(album) {
     var div = document.createElement('div');
     div.onclick = function (event) {
