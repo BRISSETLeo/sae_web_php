3a2e010c0b30e6909b78b123401b164a52d84475
ibrahim ozocak <i.ozocak45@gmail.com>
2024-02-12 16:59:37 +0100

    ajout classe Template

--- _inc/classes/Template.php
+++ _inc/classes/Template.php
@@ -10,10 +10,14 @@ class Template{
         $this->path = $path;
     }
 
-    public function render(string $contenant): string{
+    public function render(string $contenant, bool $identificationPage = false): string{
         ob_start();
         require $contenant;
         $content = ob_get_clean();
+        if(!$identificationPage){
+            require '_inc/vues/playlist.php';
+            $playlist = ob_get_clean();
+        }
         require $this->path;
         return $content;
     }
