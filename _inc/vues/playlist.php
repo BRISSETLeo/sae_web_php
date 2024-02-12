8d15dc54aff37335392ae7e9f885c89a8ce09753
ibrahim ozocak <i.ozocak45@gmail.com>
2024-02-12 16:46:21 +0100

    ajout vue menu playlist

---
+++ _inc/vues/playlist.php
@@ -0,0 +1,15 @@
+<div id="playlist-list" class="default-playlist-list">
+    <div id="flex-position">
+        <div>
+            <img id="playlist-logo" onclick="retrecir()" src="_inc/static/images/playlist.png" alt="Playlist logo"/>
+            <p class="repositionnement">Playlist</p>
+        </div>
+        <div id="resizable">
+            <img id="ajout-playlist" onclick="afficherCreationPlaylist()" src="_inc/static/images/plus.png" alt="Ajouter une playlist"/>
+            <img id="chevron" onclick="resizePlaylist()" src="_inc/static/images/next.png" alt="right chevron"/>
+        </div>
+    </div>
+</div>
+
+<link rel="stylesheet" href="_inc/static/css/playlist.css">
+<script src="_inc/static/js/resizePlaylist.js"></script>
