3b34785e4e546fdfe693e414ec6b52bab6de4965
ibrahim ozocak <i.ozocak45@gmail.com>
2024-02-12 17:04:30 +0100

    modification apporté au js default

--- _inc/static/js/default.js
+++ _inc/static/js/default.js
@@ -13,6 +13,34 @@ function afficherCreationPlaylist() {
             nomInput.value = '';
             visibilityInput.checked = false;
             descriptionInput.value = '';
+            document.removeEventListener('click', function (event) {
+                if (event.target === creationPlaylist && event.target !== playlist) {
+                    creationPlaylist.style.visibility = 'hidden';
+                    creationPlaylist.style.opacity = '0';
+                    nomInput.value = '';
+                    visibilityInput.checked = false;
+                    descriptionInput.value = '';
+                }
+            });
         }
     });
 }
+
+const popupMenu = document.getElementById('popupMenu');
+
+function afficherMenu() {
+    if (popupMenu.style.display === 'flex') {
+        popupMenu.style.display = 'none';
+    } else {
+        popupMenu.style.display = 'flex';
+    }
+}
+
+const buttonProfil = document.getElementById('button-profil');
+const spanProfil = document.getElementById('lettre-profil');
+
+document.addEventListener('click', function (event) {
+    if (popupMenu !== null && !popupMenu.contains(event.target) && event.target !== buttonProfil && event.target !== spanProfil) {
+        popupMenu.style.display = 'none';
+    }
+});
